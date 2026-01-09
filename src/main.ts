ce94892cf671511c54b7c8de7ed1490e7a1436a2
bronchitis4 <79788949+bronchitis4@users.noreply.github.com>
2026-01-10 01:27:32 +0200

    Update CORS origin URL for sportmarket-psi

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function start() {
             'http://localhost:5173',
             'http://localhost:3000',
             'https://sportmarketfront.onrender.com',
-            'https://sportmarket-psi.vercel.app/login',
+            'https://sportmarket-psi.vercel.app',
             process.env.FRONTEND_URL
         ].filter(Boolean),
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
