e47d541601ffd9d190d7b5a818433ca2e6d9f589
bronchitis4 <79788949+bronchitis4@users.noreply.github.com>
2026-01-09 23:57:45 +0200

    Fix CORS URL by removing trailing slash

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function start() {
         origin: [
             'http://localhost:5173',
             'http://localhost:3000',
-            'https://sportmarketfront.onrender.com/',
+            'https://sportmarketfront.onrender.com',
             process.env.FRONTEND_URL
         ].filter(Boolean),
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
